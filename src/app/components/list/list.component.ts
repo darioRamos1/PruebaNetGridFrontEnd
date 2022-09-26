96d4f3954ec49a37853450617ddaf8c81c57e968
Dario <darioandres08@hotmail.com>
2022-09-25 20:17:52 -0500

    Correcion de texto modal

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -8,43 +8,50 @@ import { CartaService } from 'src/app/services/carta.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
- listCartas : Carta [] = [];
- closeResult?: string;
+  listCartas: Carta[] = [];
+  closeResult?: string;
+  cartaSelect?: string = '';
 
-  constructor(private _cartaService: CartaService,
-              private modalService: NgbModal,
-              private toastr: ToastrService,) { }
+  constructor(
+    private _cartaService: CartaService,
+    private modalService: NgbModal,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.obtenerCartas();
-
   }
 
-  obtenerCartas(){
-
-    this._cartaService.getAll().subscribe(data => {
-
-      this.listCartas = data;
-    },
-    error => {
-      console.log(error);
-  })
+  obtenerCartas() {
+    this._cartaService.getAll().subscribe(
+      (data) => {
+        this.listCartas = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   open(content: any, carta: Carta) {
+    this.cartaSelect = carta.id;
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
 
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-      if(result=='Aceptar')
-      this.eliminarCarta(carta.id)
-      console.log(this.closeResult);
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      console.log(this.closeResult);
-    });
+          if (result == 'Aceptar') this.eliminarCarta(carta.id);
+          console.log(this.closeResult);
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          console.log(this.closeResult);
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
@@ -53,18 +60,22 @@ export class ListComponent implements OnInit {
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
     } else {
-      return  `with: ${reason}`;
+      return `with: ${reason}`;
     }
   }
 
-
-  eliminarCarta(id: any){
-    this._cartaService.deleteCarta(id).subscribe(data => {
-      this.toastr.success('El producto fue eliminado con exito!', 'Producto Eliminado!');
-      this.obtenerCartas();
-    },
-    error => {
-      console.log(error);
-    })
+  eliminarCarta(id: any) {
+    this._cartaService.deleteCarta(id).subscribe(
+      (data) => {
+        this.toastr.success(
+          'El producto fue eliminado con exito!',
+          'Producto Eliminado!'
+        );
+        this.obtenerCartas();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
